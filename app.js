e0b55541adfb9aaa23609ac16908373f97d4a692
unknown <arzasarvani@gmail.com>
2021-06-04 18:17:07 +0200

    updated

--- app.js
+++ app.js
@@ -8,14 +8,6 @@ hamburger.addEventListener('click', () => {
 	mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = 'rgb(116, 113, 113)';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
